df97377be0e3e51d20c6a4ad088532baf53bdae1
tamim-khan <tamim.m.khan@gmail.com>
2019-10-19 09:09:16 -0600

    add basic map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  map: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9'
+      });
+  }
+
+}
